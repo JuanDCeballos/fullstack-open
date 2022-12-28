fcbd73326dae58be7e2cad66c5b17a88554b32b8
Juan Ceballos <juandavi789@hotmail.com>
2022-12-28 09:16:27 -0500

    edit app.js

--- part1/example-app/src/App.js
+++ part1/example-app/src/App.js
@@ -1,10 +1,28 @@
-function App() {
+const Hello = (props) => {
+  const bornYear = () => {
+    const yearNow = new Date().getFullYear();
+    return yearNow - props.age;
+  };
+
   return (
-    <div className="App">
-        <p>
-          Hello world
-        </p>  
-    </div>
+    <>
+      <p>
+        hello {props.name}, you are {props.age} years old
+      </p>
+      <p>So you were probably born in {bornYear()}</p>
+    </>
+  );
+};
+
+function App() {
+  const name = "Emerson";
+  const age = 21;
+
+  return (
+    <>
+      <Hello name="Sebas" age={20}></Hello>
+      <Hello name={name} age={age}></Hello>
+    </>
   );
 }
 
