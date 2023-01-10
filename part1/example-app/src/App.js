5dfc7750d865f1aa89fee76754460eef9390ed46
Juan Ceballos <juandavi789@hotmail.com>
2023-01-09 23:26:55 -0500

    Event handling

--- part1/example-app/src/App.js
+++ part1/example-app/src/App.js
@@ -1,13 +1,30 @@
-import { useState } from "react";
+import { useState } from 'react';
 
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  setTimeout(() => setCounter(counter + 1), 1000);
+  // const handleClickLog = () => {
+  //   console.log('Clicked');
+  // };
 
-  console.log("rendering...", counter);
+  const handleClickUI = () => {
+    setCounter(counter + 1);
+  };
 
-  return <div>{`${counter} calls`}</div>;
+  const clearCounter = () => {
+    setCounter(0);
+  };
+
+  return (
+    <>
+      <div>{counter}</div>
+      {/* <button onClick={handleClickLog}>plus</button> */}
+      {/* <button onClick={() => setCounter(counter + 1)}>plus</button> */}
+      <button onClick={handleClickUI}>plus</button>
+      {/* <button onClick={() => setCounter(0)}>clear</button> */}
+      <button onClick={clearCounter}>clear</button>
+    </>
+  );
 };
 
 export default App;
