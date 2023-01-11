d5e3f5e5475dfcf69aaafb01dbba2e3236a6dc7f
Juan Ceballos <juandavi789@hotmail.com>
2023-01-10 22:25:41 -0500

    Passing state to child components

--- part1/example-app/src/App.js
+++ part1/example-app/src/App.js
@@ -1,28 +1,31 @@
 import { useState } from 'react';
 
+const Display = (props) => {
+  return <div>{props.counter}</div>;
+};
+
+const Buttons = (props) => {
+  return <button onClick={props.onClick}>{props.text}</button>;
+};
+
 const App = () => {
   const [counter, setCounter] = useState(0);
 
-  // const handleClickLog = () => {
-  //   console.log('Clicked');
-  // };
-
-  const handleClickUI = () => {
-    setCounter(counter + 1);
-  };
-
-  const clearCounter = () => {
-    setCounter(0);
-  };
+  const increaseCounter = () => setCounter(counter + 1);
+  const decreaseCounter = () => setCounter(counter - 1);
+  const clearCounter = () => setCounter(0);
 
   return (
     <>
-      <div>{counter}</div>
+      <Display counter={counter} />
       {/* <button onClick={handleClickLog}>plus</button> */}
       {/* <button onClick={() => setCounter(counter + 1)}>plus</button> */}
-      <button onClick={handleClickUI}>plus</button>
+      {/* <button onClick={increaseCounter}>plus</button> */}
       {/* <button onClick={() => setCounter(0)}>clear</button> */}
-      <button onClick={clearCounter}>clear</button>
+      {/* <button onClick={clearCounter}>clear</button> */}
+      <Buttons onClick={increaseCounter} text='plus' />
+      <Buttons onClick={clearCounter} text='clear' />
+      <Buttons onClick={decreaseCounter} text='decrease' />
     </>
   );
 };
